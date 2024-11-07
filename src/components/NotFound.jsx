78a52df26a8c1c8b0fa9f70207c25623329332ad
dafy.010 <dafy.010@iMac-de-dafy010.local>
2024-11-07 10:35:30 +0100

    not found page styles

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,10 +6,12 @@ import "../styles/layout/NotFound.scss";
 const NotFound = () => {
   return (
     <div className="main--img">
-
+      <div className='notfound--wrap'>
       <h1 className='notfound__title--text'>PAGE NOT FOUND</h1>
       <Link className="notfound__link" to="/">HOME</Link>
+      </div>
 
+    
     </div>
   );
 }
