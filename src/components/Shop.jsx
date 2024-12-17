1dfae1ff2c260ee647529bb8eeb6542c8e03d551
dafy.010 <dafy.010@iMac-de-dafy010.local>
2024-12-17 11:34:23 +0100

    new shop display

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Header2 from './Header2';
 import "../styles/layout/Shop.scss";
-
 
 function Shop() {
   return (
     <>
-     <div className="dossier--link__shop">
-        <div className="div--shop">
-            <a className="link--shop" href="http://www.entrebotones.com/?s=hermana+furia&post_type=product" target='about_blank' rel='noopener noreferrer'> <i class="fa-solid fa-cart-shopping"></i>COMPRA AHORA</a>
-        </div>
+      {/* Contenedor de la tienda embebida */}
+      <div className="iframe--container">
+        <iframe
+          src="https://www.entrebotones.com/?s=hermana+furia&post_type=product"
+          title="Tienda Hermana Furia"
+          allowFullScreen
+        ></iframe>
       </div>
+
+      {/* Contenedor de fondo */}
       <div className="main--img__nb">
-        <Header2/>
+        <Header2 />
       </div>
     </>
-  )
+  );
 }
 
-export default Shop
+export default Shop;
