321a3f43512ccbb4d9ce8f0535a2b22257afd42a
dafy.010 <dafy.010@iMac-de-dafy010.local>
2024-12-17 12:46:16 +0100

    save dist

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div className="main--img__landing">
         <video autoPlay muted loop className="background-video">
-        <source src="video/Hermana_Furia_B1.mp4" type="video/mp4" />
+        <source src="/hermana-furia/video/Hermana_Furia_B1.mp4" type="video/mp4" />
         Tu navegador no soporta videos en formato MP4.
       </video>
       <h1 className='main-title'>HERMANA FURIA</h1>
