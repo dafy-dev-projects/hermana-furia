3cb95ba2fa2860ecba87721ebc771a9b9daac379
dafy.010 <dafy.010@iMac-de-dafy010.local>
2024-12-17 13:08:01 +0100

    fixes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header2 from './Header2';
+import Header2 from './Header2'
 
 import "../styles/layout/Landing.scss";
 
@@ -7,9 +7,9 @@ const Landing = () => {
   return (
     <div className="main--img__landing">
         <video autoPlay muted loop className="background-video">
-      <source src="/video/Hermana_Furia_B1.mp4" type="video/mp4" />
-      Tu navegador no soporta videos en formato MP4.
-    </video>
+        <source src="./public/video/Hermana_Furia_B1.mp4" type="video/mp4" />
+        Tu navegador no soporta videos en formato MP4.
+      </video>
       <h1 className='main-title'>HERMANA FURIA</h1>
 
       <Header2 />
